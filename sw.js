ab847654a284a463e380c63fa57ba1d6da13de9c
lololol1000 <anemailisthis1000@gmail.com>
2025-05-28 10:26:45 -0700

    oo

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ var _cacheFiles = [
 	'RSDKv5.html',
 	'RSDKv5.js',
 	'RSDKv5.wasm',
-  'https://archive.org/download/data-rsdk-sonic-mania/Data.rsdk'
+  'Data.rsdk'
 ];
 	
 self.addEventListener('install', (e) => {
